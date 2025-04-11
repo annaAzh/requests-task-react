b7655cb763311c99f6d90a18bc490bb4bdfcbd23
annaAzh <forarre@yandex.ru>
2025-04-11 13:17:07 +0300

    feat: update not found page

--- src/pages/notFound/ui/notFound.tsx
+++ src/pages/notFound/ui/notFound.tsx
@@ -1,9 +1,18 @@
+import { Paths } from '@/shared/types';
+import { Button } from '@/shared/components';
+import { useNavigate } from 'react-router';
+
 export const NotFound = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(Paths.REQUESTS);
+  };
   return (
-    <>
+    <div className="flex flex-col justify-center items-center grow">
       <div className="text-6xl mb-4">
-        404 <span className="text-3xl">page</span>
+        404 <span className="text-3xl">страница</span>
       </div>
-    </>
+      <Button onClick={handleClick}>Вернуться на главную</Button>
+    </div>
   );
 };
